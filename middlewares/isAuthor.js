a68110840492bb0bcc6f1a0dfa6eb48f40a1b847
nhussein2026 <naser738nas@gmail.com>
2022-10-31 16:58:46 +0300

    Adding is author

---
+++ middlewares/isAuthor.js
@@ -0,0 +1,14 @@
+const isAuthor = function(req, res, next) {
+    if (req.user) {
+        if (req.user.type == 'author' ||    "admin") {
+            return next()
+        }
+    }
+    res.status(403)
+    res.send({
+        success: false,
+        messages: ['Youuu do not have permission to perform this action']
+    })
+}
+
+module.exports = isAuthor
