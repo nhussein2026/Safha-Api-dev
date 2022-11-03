d020ae5fe32ad6cdc470dc4e026f70afaded8e01
Manalei97 <manal.ye.tr@gmail.com>
2022-11-03 17:42:03 +0300

    edit admin and users controller

--- models/user.js
+++ models/user.js
@@ -11,9 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.belongsTo(models.UserType, {
-        foreignKey: 'id',
-      });
+      User.belongsTo(models.UserType);
       User.hasMany(models.comment, {
         foreignKey: 'id',
       })
