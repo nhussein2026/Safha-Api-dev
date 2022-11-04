7b32c57d2fe21dec13463c2ab7d6f5140721d5a6
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-04 07:36:44 -0400

    add photoTransformer

---
+++ transformers/photoTransformer.js
@@ -0,0 +1,13 @@
+// var photoTransformer = function(photo) {
+//     photo.file = 'http://localhost:3000/uploads/' + photo.file
+// photo.file = 'http://192.168.122.5:3000/uploads/' + photo.file
+//     return photo
+// }
+// var photosTransformer = function(photos) {
+//     return photos.map(photo => photoTransformer(photo))
+// }
+
+// module.exports = {
+//     photoTransformer,
+//     photosTransformer
+// }
