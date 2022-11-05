bf771a5928fa8fe6ba7d2d67fa3dd9303c1bb1cb
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-05 08:42:19 -0400

    edit book and admin controller

--- transformers/bookTransformer.js
+++ transformers/bookTransformer.js
@@ -10,9 +10,10 @@ var photoTransformer = function(photo) {
     console.log("process.env.REACT_APP_API_URL",process.env.REACT_APP_API_URL)
     // photo.file = 'http://localhost:3000/uploads/' + photo.file
     // photo = `${process.env.REACT_APP_API_URL}/uploads/` + photo
-    // var ghadirPath = "http://192.168.122.5:3000/api/v1"
-    var SafhaPath = "http://localhost:3000/api/v1"
-    photo = `${SafhaPath}/uploads/` + photo
+    // var SafhaPath = "http://192.168.122.5:3000/api/v1"
+    // var SafhaPath = "http://localhost:3000/api/v1"
+    // photo = `${SafhaPath}/uploads/` + photo
+    photo = `/uploads/` + photo
     return photo
 }
 // var photosTransformer = function(photos) {
