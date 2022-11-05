b1e97f31cec1bb304b6f2442f3558c6a0cb1e447
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-05 03:37:59 -0400

    photoTransformer and gitignore

--- transformers/photoTransformer.js
+++ transformers/photoTransformer.js
@@ -1,13 +1,13 @@
-// var photoTransformer = function(photo) {
-//     photo.file = 'http://localhost:3000/uploads/' + photo.file
-// photo.file = 'http://192.168.122.5:3000/uploads/' + photo.file
-//     return photo
-// }
-// var photosTransformer = function(photos) {
-//     return photos.map(photo => photoTransformer(photo))
-// }
+var photoTransformer = function(photo) {
+    // photo.file = 'http://localhost:3000/uploads/' + photo.file
+    photo.file = process.env.REACT_APP_API_URL + '/uploads/' + photo.file
+    return photo
+}
+var photosTransformer = function(photos) {
+    return photos.map(photo => photoTransformer(photo))
+}
 
-// module.exports = {
-//     photoTransformer,
-//     photosTransformer
-// }
+module.exports = {
+    photoTransformer,
+    photosTransformer
+}
