cdcb82d8bd123d90b3a588223129e4fe770a5855
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-05 05:06:33 -0400

    edit book transformer

--- transformers/bookTransformer.js
+++ transformers/bookTransformer.js
@@ -1,13 +1,18 @@
-var bookTransformer = function(book) {
-    if (member?.dataValues?.cover) {
-        member.dataValues.cover = photoTransformer(cover)
+var bookTransformer = function(book){
+    console.log("inside book transformer")
+    if (book?.dataValues?.cover) {
+        book.dataValues.cover = photoTransformer(book.dataValues.cover)
     }
     return book
 }
 
 var photoTransformer = function(photo) {
+    console.log("process.env.REACT_APP_API_URL",process.env.REACT_APP_API_URL)
     // photo.file = 'http://localhost:3000/uploads/' + photo.file
-    photo.file = process.env.REACT_APP_API_URL + '/uploads/' + photo.file
+    // photo = `${process.env.REACT_APP_API_URL}/uploads/` + photo
+    // var ghadirPath = "http://192.168.122.5:3000/api/v1"
+    var SafhaPath = "http://localhost:3000/api/v1"
+    photo = `${SafhaPath}/uploads/` + photo
     return photo
 }
 // var photosTransformer = function(photos) {
@@ -16,5 +21,6 @@ var photoTransformer = function(photo) {
 
 module.exports = {
     photoTransformer,
+    bookTransformer
     // photosTransformer
 }
