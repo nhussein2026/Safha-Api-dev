a30603c2bea7652d26c367884bf881281e4b555a
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-05 04:27:51 -0400

    bookTransformer for cover

---
+++ transformers/bookTransformer.js
@@ -0,0 +1,19 @@
+var bookTransformer = function(book) {
+    if (member?.dataValues?.cover) {
+        member.dataValues.cover = photoTransformer(cover)
+    }
+}
+
+var photoTransformer = function(photo) {
+    // photo.file = 'http://localhost:3000/uploads/' + photo.file
+    photo.file = process.env.REACT_APP_API_URL + '/uploads/' + photo.file
+    return photo
+}
+// var photosTransformer = function(photos) {
+//     return photos.map(photo => photoTransformer(photo))
+// }
+
+module.exports = {
+    photoTransformer,
+    // photosTransformer
+}
