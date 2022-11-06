aa1bc14b2539fd2e46d2dab8a2d37fb1761db989
nhussein2026 <naser738nas@gmail.com>
2022-11-06 22:52:33 +0300

    fixed ISBN column

--- migrations/20221106135912-add_ISBN_column_to_books.js
+++ migrations/20221106135912-add_ISBN_column_to_books.js
@@ -10,9 +10,7 @@ module.exports = {
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
      await queryInterface.addColumn('books', 'ISBN', {
-      ISBN: {
         type: Sequelize.STRING
-      }, 
     });
   },
 
