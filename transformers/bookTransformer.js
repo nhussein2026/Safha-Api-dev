d152bb133141564085ef389d3c8296c9447175af
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-07 04:15:07 -0500

    edit book controller and book transformer

--- transformers/bookTransformer.js
+++ transformers/bookTransformer.js
@@ -1,5 +1,9 @@
+
+var booksTransformer = function(books) {
+    return books.map(book => bookTransformer(book))
+}
+
 var bookTransformer = function(book){
-    console.log("inside book transformer")
     if (book?.dataValues?.cover) {
         book.dataValues.cover = photoTransformer(book.dataValues.cover)
     }
@@ -7,21 +11,12 @@ var bookTransformer = function(book){
 }
 
 var photoTransformer = function(photo) {
-    console.log("process.env.REACT_APP_API_URL",process.env.REACT_APP_API_URL)
-    // photo.file = 'http://localhost:3000/uploads/' + photo.file
-    // photo = `${process.env.REACT_APP_API_URL}/uploads/` + photo
-    // var SafhaPath = "http://192.168.122.5:3000/api/v1"
-    // var SafhaPath = "http://localhost:3000/api/v1"
-    // photo = `${SafhaPath}/uploads/` + photo
-    photo = `/uploads/` + photo
+    photo = process.env.API_URL + '/uploads/' + photo
     return photo
 }
-// var photosTransformer = function(photos) {
-//     return photos.map(photo => photoTransformer(photo))
-// }
 
 module.exports = {
     photoTransformer,
-    bookTransformer
-    // photosTransformer
+    bookTransformer,
+    booksTransformer
 }
